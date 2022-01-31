bd67cecb0ceaf2f21b8aebabdfbc7253c586db61
Rajiv Gupta <rajivgupta9868@gmail.com>
2022-01-31 18:04:01 +0530

    day 15

--- zee5appjdbc/src/main/java/com/zee/zee5app/config/Config.java
+++ zee5appjdbc/src/main/java/com/zee/zee5app/config/Config.java
@@ -31,7 +31,7 @@ public class Config {
 		basicDataSource.setUsername(environment.getProperty("jdbc.username"));
 		basicDataSource.setPassword(environment.getProperty("jdbc.password"));
 		basicDataSource.setUrl(environment.getProperty("jdbc.url"));
-		
+		basicDataSource.setDefaultAutoCommit(false);
 		return basicDataSource;
 		
 	}
